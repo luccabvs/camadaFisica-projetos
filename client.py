9225e19265bf1e5c7ecd0235721d1b9ccfabd706
josermf2 <josermf2@al.insper.edu.br>
2021-09-01 18:49:26 -0300

    finaliza projeto2

--- client.py
+++ client.py
@@ -1,36 +1,54 @@
 import random
 from datetime import datetime
+from sys import byteorder
 from enlace import *
 import numpy as np
 
+def generate_commandList():
+    commandList = [b"\xFF", b"\x00", b"\x0F", b"\xF0", b"\xFF"b"\x00", b"\x00"b"\xFF"]
+    listLenght = random.randint(10,30)
+    listBytes = []
+    listBytesWithoutFlags = []
+    for lenght in range(listLenght):
+        command = random.choice(commandList)
+        if len(command) == 2:
+            listBytes.append(b"\xAA")
+        listBytes.append(command)
+        listBytesWithoutFlags.append(command)
+    listBytes.append(b"\xBB")
+    listBytes_to_Array = b"".join(listBytes)
+    return listBytes_to_Array, listBytesWithoutFlags
+
 def main():
     try:
-        com1 = enlace('/dev/ttyACM0')
+        com1 = enlace("/dev/ttyACM0")
 
         com1.enable()
+        
+        com1.sendData(b"\xdd")
 
-        commandList = [b'00FF', b'00', b'0F', b'F0', b'FF00', b'FF']
+        txBuffer, listBytesWithoutFlags = generate_commandList()
 
-        listLenght = random.randint(10,30)
+        time.sleep(1)
 
-        listBytes = []
+        com1.sendData(np.asarray(txBuffer))
+        print(f'Tamanho da lista enviada: {len(listBytesWithoutFlags)}')
 
-        initTime = datetime.now()
+        while True:
+            lenght = com1.getData(1)
+            if len(lenght) > 0:
+                received = int.from_bytes(lenght[0], byteorder='big')
+                print('Tamanho da lista recebida: {}'.format(received))
+                break
 
-        for lenght in range(listLenght):
-            listBytes.append(random.choice(commandList))
-
-        com1.sendData(np.array(listBytes))
-        print(np.array(listBytes))
-        print('deu bom')
         com1.disable()
+
 
     except Exception as erro:     
-        print('F')
+        print("F")
         print(erro) 
         com1.disable()
-   
 
 
 if __name__ == "__main__":
-    main()
+    main()   
