f02ebec811434c5bc0c4c0c4de5c1a858ddd468f
josermf2 <josermf2@al.insper.edu.br>
2021-11-11 15:30:12 -0300

    finaliza encode projeto 8

--- projeto-8/encode_p8.py
+++ projeto-8/encode_p8.py
@@ -2,11 +2,18 @@ from suaBibSignal import signalMeu
 from funcoes_LPF import LPF
 from scipy.io import wavfile
 import sounddevice as sd
+import soundfile   as sf
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     signal = signalMeu()
-    
+
     samplerate, data = wavfile.read('/home/josermf/Insper/4oSemestre/CamadaFisica/camadaFisica-projetos/projeto-8/audio.wav')
+
+    duration = int(len(data)/samplerate)
+
+    t = np.linspace(0, duration, duration*samplerate)
 
     max_data = max([abs(i) for i in data])
 
@@ -14,8 +21,25 @@ def main():
 
     for i in data:
         normalized_data.append(i/max_data)
-
+    
+    plt.figure()
+    plt.plot(t, normalized_data[:len(t)])
+    plt.ylabel('Áudio Normalizado')
+    plt.xlabel('Tempo')
+    plt.grid()
+    plt.title('Áudio Normalizado vs Tempo')
+    
     filtred_data = LPF(normalized_data, 4000, samplerate)
+
+    plt.figure()
+    plt.plot(t, filtred_data[:len(t)])
+    plt.ylabel('Áudio Filtrado')
+    plt.xlabel('Tempo')
+    plt.grid()
+    plt.title('Áudio Filtrado vs Tempo')
+
+    signal.plotFFT(filtred_data, samplerate)
+    plt.title('Áudio Filtrado vs Frequencia')
 
     portadora = 14000
 
@@ -24,12 +48,25 @@ def main():
     x = x[0:len(filtred_data)]
     y = y[0:len(filtred_data)]
 
-    audioModulado = y*filtred_data
-    audioDemodulado = y*audioModulado
+    audio_modulado = y*filtred_data
+    audio_Demodulado = y*audio_modulado
 
-    sd.play(audioModulado, samplerate)
+    plt.figure()
+    plt.plot(t, audio_modulado[:len(t)])
+    plt.ylabel('Áudio Modulado')
+    plt.xlabel('Tempo')
+    plt.grid()
+    plt.title('Áudio Modulado vs Tempo')
+
+    signal.plotFFT(audio_modulado, samplerate)
+    plt.title('Áudio Modulado vs Frequencia')
+
+    sd.play(audio_Demodulado, samplerate)
+    sf.write('new_audio.wav', audio_modulado, samplerate)
 
     sd.wait()
 
+    plt.show()
+
 if __name__ == "__main__":
-    main()
+    main() 
